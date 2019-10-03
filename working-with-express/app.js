828a2cf5f558c91af319bc82f946ac1a4462de70
Jay T <jaytula@gmail.com>
2019-10-03 13:19:27 -0700

    Parse Incoming Requests

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -1,20 +1,32 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}))
 app.use((req, res, next) => {
-    console.log('This middleware always runs');
-    next();
-})
+  //console.log("This middleware always runs");
+  next();
+});
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In the another middleware');
-    res.send('<h1>Add product</h1>');
-})
+app.use("/add-product", (req, res, next) => {
+  //console.log("In the another middleware");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
 
-app.use('/', (req, res, next) => {
-    console.log('In the middleware');
-    res.send('<h1>Hello from Express!</h1>')
-})
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
-app.listen(3000);
+app.use("/", (req, res, next) => {
+  //console.log("In the middleware");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});

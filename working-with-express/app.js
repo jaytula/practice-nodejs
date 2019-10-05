cf0de25d10e253057f279332b2ccae4e9c36ec8c
Jay T <jaytula@gmail.com>
2019-10-05 10:27:25 -0700

    Using Express Router

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -1,30 +1,14 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
-
 app.use(bodyParser.urlencoded({extended: false}))
-app.use((req, res, next) => {
-  //console.log("This middleware always runs");
-  next();
-});
 
-app.use("/add-product", (req, res, next) => {
-  //console.log("In the another middleware");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  //console.log("In the middleware");
-  res.send("<h1>Hello from Express!</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {

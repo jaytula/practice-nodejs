dded81b9ba270200519a0872d4659c2a3d290b43
Jay T <jaytula@gmail.com>
2019-10-09 17:21:31 -0700

    Adding a 404 Error Page

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -10,6 +10,9 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res) => {
+  res.status(404).send('<h1>Page not found</h1>');
+})
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);

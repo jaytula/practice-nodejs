2f80d7e4e16c6622755f034f5988a4cb0bf66170
Jay T <jaytula@gmail.com>
2019-10-10 10:27:00 -0700

    Serving HTML Pages

--- working-with-express/routes/admin.js
+++ working-with-express/routes/admin.js
@@ -1,17 +1,16 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
 
 const router = express.Router();
 
-router.get("/add-product", (req, res, next) => {
+router.get('/add-product', (req, res, next) => {
   //console.log("In the another middleware");
-  res.send(
-    '<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-router.post("/product", (req, res, next) => {
+router.post('/product', (req, res, next) => {
   console.log(req.body);
-  res.redirect("/");
+  res.redirect('/');
 });
 
 module.exports = router;

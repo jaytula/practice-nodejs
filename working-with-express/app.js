c5e3f265ecfd45c2bbf275dd992f69cef125f1e3
Jay T <jaytula@gmail.com>
2019-10-10 10:29:23 -0700

    Returning a  404 page

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -1,18 +1,19 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.status(404).send('<h1>Page not found</h1>');
-})
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+});
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);

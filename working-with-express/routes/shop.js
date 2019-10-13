acc0fd028074897a7cb4495e37fb46d5bacb7629
Jay T <jaytula@gmail.com>
2019-10-13 08:59:02 -0700

    Converting our Project to Handlebars

--- working-with-express/routes/shop.js
+++ working-with-express/routes/shop.js
@@ -7,7 +7,7 @@ const adminData = require('./admin');
 const router = express.Router();
 router.get("/", (req, res, next) => {
     const products = adminData.products;
-    res.render('shop', { pageTitle: 'Shop', path: '/', prods: products, docTitle: 'Shop'});
+    res.render('shop', { pageTitle: 'Shop', path: '/', prods: products, hasProducts: !!products.length,  docTitle: 'Shop'});
   });
 
 module.exports = router;

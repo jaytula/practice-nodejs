ddda752cd9cc85be62546646995bb76e1c16802a
Jay Tulathimutte <jaytula@gmail.com>
2019-10-15 15:52:50 -0700

    Move static middleware up

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -7,12 +7,12 @@ const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);

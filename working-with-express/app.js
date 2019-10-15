9cba90fcbaa4fb79d91ef461e979729535344b9d
Jay T <jaytula@gmail.com>
2019-10-15 09:20:10 -0700

    Finishing the Controllers

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -17,9 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res) => {
-  res.status(404).render('404', {pageTitle: 'Not Found', path: null});
-});
+app.use(errorController.get404);
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);

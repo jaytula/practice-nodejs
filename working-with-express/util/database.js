a5eb807b82f89efec56041026a57bdf958754354
Jay T <jaytula@gmail.com>
2019-10-21 10:14:17 -0700

    sequelize: Connecting to the Database

--- working-with-express/util/database.js
+++ working-with-express/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'example',
-    database: 'node-complete'
+const sequelize = new Sequelize('node-complete', 'root', 'example', {
+  host: 'localhost',
+  dialect: 'mysql'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;

b2e44f78729e54ed06cca358a0b693c7b0bb75cb
Jay T <jaytula@gmail.com>
2019-10-22 11:37:23 -0700

    Creating and Managing a Dummy User

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -12,10 +12,19 @@ const Product = require('./models/product');
 const User = require('./models/user');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -24,12 +33,24 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true})
+  .sync()
   .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({
+        name: 'dummy',
+        email: 'dummy@example.com'
+      });
+    }
+    return user;
+  })
+  .then(() => {
     const PORT = 3000;
     app.listen(PORT, () => {
       console.log(`Listening on ${PORT}`);

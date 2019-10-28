2e1f76fca6cf12bb358765edd0bc1cda7c96044f
Jay T <jaytula@gmail.com>
2019-10-28 09:27:21 -0700

    Storing Cartitems as Orderitems

--- working-with-express/controllers/shop.js
+++ working-with-express/controllers/shop.js
@@ -1,4 +1,6 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
+const OrderItem = require('../models/order-item');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
@@ -109,6 +111,30 @@ exports.postCartDeleteProduct = (req, res, next) => {
     })
     .then(result => {
       res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  let order;
+
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts();
+    })
+    .then(products => {
+      return req.user.createOrder().then(order => {
+        return order.addProducts(
+          products.map(product => {
+            product.orderItem = { quantity: product.cartItem.quantity };
+            return product;
+          })
+        );
+      });
+    })
+    .then(() => {
+      res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };

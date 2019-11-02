c0543279f1150399b9f831ee7da2bebc90a186ce
Jay T <jaytula@gmail.com>
2019-11-01 20:20:52 -0700

    Working on Cart Items and Orders

--- working-with-express/models/user.js
+++ working-with-express/models/user.js
@@ -1,26 +1,36 @@
 const mongodb = require('mongodb');
-const {getDb} = require('../util/database');
+const { getDb } = require('../util/database');
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
 
-    return db
-      .collection('users')
-      .insertOne(this);
+    return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    db.collection('users').updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findById(id) {
     const db = getDb();
 
-    return db
-      .collection('users')
-      .findOne({ _id: new mongodb.ObjectId(id) });
+    return db.collection('users').findOne({ _id: new mongodb.ObjectId(id) });
   }
 }
 module.exports = User;

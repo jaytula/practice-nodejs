0c3c345ca6569b88d9fd84c4b25bee246342b055
Jay T <jaytula@gmail.com>
2019-11-10 09:04:56 -0800

    Adding and Using a User Model

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -11,6 +11,8 @@ const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const User = require('./models/user');
+
 const PORT = 3001;
 const app = express();
 
@@ -18,14 +20,14 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use((req, res, next) => {
-//   User.findById('5dbccfd61c9d44000065ed74')
-//     .then(user => {
-//       req.user = new User(user.username, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('5dc8428408cfb3065e2618da')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -34,8 +36,31 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(result => { 
-  app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
-  });
-}).catch(err => console.log(err))
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(result => {
+    return User.findOne({ email: 'jay@test.com' });
+  })
+  .then(user => {
+    if (!user) {
+      const user = new User({
+        name: 'Jay',
+        email: 'jay@test.com',
+        cart: {
+          items: []
+        }
+      });
+
+      return user.save();
+    }
+    return null;
+  })
+  .then(result => {
+    app.listen(PORT, () => {
+      console.log(`Listening on ${PORT}`);
+    });
+  })
+  .catch(err => console.log(err));

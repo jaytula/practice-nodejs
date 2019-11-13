b1059295e013cd1d1feb796de44e8fd0d5650690
Jay T <jaytula@gmail.com>
2019-11-13 08:35:14 -0800

    Creating the Login Form

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -10,6 +10,7 @@ const errorController = require('./controllers/error');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 const User = require('./models/user');
 
@@ -33,6 +34,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
---
+++ working-with-express/controllers/auth.js
@@ -0,0 +1,3 @@
+exports.getLogin = (req, res) => {
+    res.render('auth/login', {path: '/login', pageTitle: "Login"});
+}

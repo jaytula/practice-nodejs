dd84032e2f17b66a418552488739fc14e56de524
Jay T <jaytula@gmail.com>
2019-11-26 21:59:27 -0800

    Adding Pagination: Retrieving a Chunk of Data

--- working-with-express/controllers/shop.js
+++ working-with-express/controllers/shop.js
@@ -6,6 +6,8 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 const User = require('../models/user');
 
+const ITEMS_PER_PAGE = 2;
+
 const appDir = path.dirname(require.main.filename);
 
 exports.getProducts = (req, res, next) => {
@@ -42,7 +44,9 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.find()
+  const page = req.query.page || 1;
+
+  Product.find().skip((page-1)*ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
     .then(products => {
       res.render('shop/index', {
         pageTitle: 'Shop',

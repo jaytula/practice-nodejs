9e749c03a2adc21071915aee3cb712075b731310
Jay T <jaytula@gmail.com>
2019-11-30 09:11:37 -0800

    Disable getCheckoutSuccess

--- working-with-express/routes/shop.js
+++ working-with-express/routes/shop.js
@@ -15,7 +15,7 @@ router.post('/create-order', isAuth, shopController.postOrder);
 router.get('/orders', isAuth, shopController.getOrders);
 router.get('/orders/:orderId', isAuth, shopController.getInvoice)
 router.get('/checkout', isAuth, shopController.getCheckout);
-router.get('/checkout/success', shopController.getCheckoutSuccess);
+//router.get('/checkout/success', shopController.getCheckoutSuccess);
 router.get('/checkout/cancel', shopController.getCheckout);
 
 

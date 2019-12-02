ca113ee2750e7537b035058f38a59b15c69922e2
Jay T <jaytula@gmail.com>
2019-12-01 23:34:24 -0800

    Remove commented code

--- working-with-express/assignment2.js
+++ working-with-express/assignment2.js
@@ -2,20 +2,6 @@ const express = require("express");
 
 const app = express();
 
-// app.use((req, res, next) => {
-//     console.log('middleware 1');
-//     next();
-// });
-
-// app.use((req, res, next) => {
-//     console.log('middleware 2');
-//     next();
-// })
-
-// app.use((req, res, next) => {
-//     res.send('hi there');
-// })
-
 app.use("/users", (req, res, next) => {
   res.send("/users middleware");
 });

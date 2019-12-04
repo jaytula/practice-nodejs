4d47661503c67a71291bf5942a676d597ab1ef3a
Jay T <jaytula@gmail.com>
2019-12-03 17:05:47 -0800

    REST APIs: Storing Posts in the Database

--- rest-project/backend/controllers/feed.js
+++ rest-project/backend/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -18,30 +20,32 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
-
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: 'Validation failed, entered data is incorrect',
-        errors: errors.array()
-      });
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect',
+      errors: errors.array()
+    });
   }
+
+  const title = req.body.title;
+  const content = req.body.content;
   // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully',
-    post: {
-      id: new Date().toISOString(),
-      title,
-      content,
-      creator: {
-        name: 'Jay'
-      },
-      createdAt: new Date()
+  const post = new Post({
+    title,
+    content,
+    imageUrl: 'images/duck.jpg',
+    creator: {
+      name: 'Jay'
     }
+  });
+
+  post.save().then(result => {
+    console.log(result);
+    res.status(201).json({
+      message: 'Post created successfully',
+      post: result
+    });
   });
 };

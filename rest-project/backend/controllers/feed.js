d76616f860451b9a1f430d26673038e7082e4f37
Jay T <jaytula@gmail.com>
2019-12-04 12:24:35 -0800

    Remove commented code

--- rest-project/backend/controllers/feed.js
+++ rest-project/backend/controllers/feed.js
@@ -94,12 +94,6 @@ exports.updatePost = (req, res, next) => {
     throw error;
   }
 
-  // if (!req.file) {
-  //   const error = new Error('No image provided.');
-  //   error.statusCode = 422;
-  //   throw error;
-  // }
-
   const title = req.body.title;
   const content = req.body.content;
   const imageUrl = req.file ? req.file.path : req.body.image;
@@ -134,22 +128,6 @@ exports.updatePost = (req, res, next) => {
       if (!err.statusCode) err.statusCode = 500;
       next(err);
     });
-
-  // post
-  //   .save()
-  //   .then(result => {
-  //     console.log(result);
-  //     res.status(201).json({
-  //       message: 'Post created successfully',
-  //       post: result
-  //     });
-  //   })
-  //   .catch(err => {
-  //     if (!err.statusCode) {
-  //       err.statusCode = 500;
-  //     }
-  //     next(err);
-  //   });
 };
 
 const clearImage = filePath => {

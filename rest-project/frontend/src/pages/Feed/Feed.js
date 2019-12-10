41feb0dd50e2dd09cf93dc175adf282c3e29c26f
Jay T <jaytula@gmail.com>
2019-12-10 14:39:42 -0800

    Websockets: Updating Posts On All Connected Clients

--- rest-project/frontend/src/pages/Feed/Feed.js
+++ rest-project/frontend/src/pages/Feed/Feed.js
@@ -46,6 +46,9 @@ class Feed extends Component {
         case 'create':
           this.addPost(data.post);
           break;
+        case 'update':
+          this.updatePost(data.post);
+          break;
         default:
           console.log(`Action ${data.action} not implemented`);
       }
@@ -62,6 +65,19 @@ class Feed extends Component {
       return {
         posts: updatedPosts,
         totalPosts: prevState.totalPosts + 1
+      };
+    });
+  };
+
+  updatePost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id);
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts
       };
     });
   };
@@ -179,15 +195,7 @@ class Feed extends Component {
           createdAt: resData.post.createdAt
         };
         this.setState(prevState => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          }
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false

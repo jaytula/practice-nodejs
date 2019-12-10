7b3795adbbb39d963d11a042a00ace5f8f499b82
Jay T <jaytula@gmail.com>
2019-12-10 14:41:15 -0800

    Websockets: Sorting Correctly

--- rest-project/backend/controllers/feed.js
+++ rest-project/backend/controllers/feed.js
@@ -17,6 +17,7 @@ exports.getPosts = async (req, res, next) => {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
       .populate('creator')
+      .sort({createdAt: -1})
       .skip((currentPage - 1) * POSTS_PER_PAGE)
       .limit(POSTS_PER_PAGE);
 

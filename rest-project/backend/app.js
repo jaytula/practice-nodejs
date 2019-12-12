a64ddbc39d8087c344ee6f724d1b280fbd7c1deb
Jay T <jaytula@gmail.com>
2019-12-12 09:42:41 -0800

    Change per deprecation

--- rest-project/backend/app.js
+++ rest-project/backend/app.js
@@ -56,7 +56,7 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
-    formatError(err) {
+    customFormatErrorFn(err) {
       if(!err.originalError) {
         return err;
       }

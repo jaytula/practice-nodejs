0480b3770f8b91129e18590c529d158e5cc5d5a2
Jay Tulathimutte <jaytula@gmail.com>
2019-12-18 14:21:23 -0800

    Use graphql variable for Singel Post query

--- rest-project/frontend/src/pages/Feed/SinglePost/SinglePost.js
+++ rest-project/frontend/src/pages/Feed/SinglePost/SinglePost.js
@@ -18,8 +18,8 @@ class SinglePost extends Component {
     const postId = this.props.match.params.postId;
     const graphqlQuery = {
       query: `
-      {
-        post(id: "${postId}") {
+      query GetPost($id: ID!) {
+        post(id: $id) {
           _id
           title
           content
@@ -30,10 +30,16 @@ class SinglePost extends Component {
           createdAt
         }
       }
-      `
-    }
+      `,
+      variables: {
+        id: postId
+      }
+    };
     fetch(`${BACKEND}/graphql`, {
-      headers: { Authorization: `Bearer ${this.props.token}`, 'Content-Type': 'application/json' },
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+        'Content-Type': 'application/json'
+      },
       method: 'POST',
       body: JSON.stringify(graphqlQuery)
     })
@@ -41,14 +47,16 @@ class SinglePost extends Component {
         return res.json();
       })
       .then(resData => {
-        if(resData.errors) {
+        if (resData.errors) {
           throw new Error('Single post retrieval failed!');
         }
         this.setState({
           title: resData.data.post.title,
           author: resData.data.post.creator.name,
           image: `${BACKEND}/${resData.data.post.imageUrl}`,
-          date: new Date(resData.data.post.createdAt).toLocaleDateString('en-US'),
+          date: new Date(resData.data.post.createdAt).toLocaleDateString(
+            'en-US'
+          ),
           content: resData.data.post.content
         });
       })
@@ -59,12 +67,12 @@ class SinglePost extends Component {
 
   render() {
     return (
-      <section className='single-post'>
+      <section className="single-post">
         <h1>{this.state.title}</h1>
         <h2>
           Created by {this.state.author} on {this.state.date}
         </h2>
-        <div className='single-post__image'>
+        <div className="single-post__image">
           <Image contain imageUrl={this.state.image} />
         </div>
         <p>{this.state.content}</p>
